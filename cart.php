d6ae3bba5fbb52dbd97a10baa14785aba5da1e15
AakritiChopra <77447057+AakritiChopra@users.noreply.github.com>
2021-07-23 17:02:22 +0530

    Update cart.php

--- cart.php
+++ cart.php
@@ -25,7 +25,7 @@ require('top.php');
             <div class="container">
                 <div class="row">
                     <div class="col-md-12 col-sm-12 col-xs-12">
-                        <form action="#">               
+                        <form action="checkout.php" method="POST">               
                             <div class="table-content table-responsive">
                                 <table>
                                     <thead>
